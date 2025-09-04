cca9765bdb4d1b1878a767312041a5071de42764
skyesahawat2-glitch <skye.sahawat2@gmail.com>
2025-09-04 18:43:40 +0700

    S.H.A.P.cpp code update
    update: access_dmm
    update: username/password = exit
    update:pjmm exit

---
+++ S.H.A.P.cpp
@@ -0,0 +1,370 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+int main (void)
+{
+	printf("------------system online-----------\n");
+  char user[10], pass[10],direction2[12],direction3[3],direction5[20],name1[20],surename1[20],direction12[20];
+  char user1[]="admin",pass1[]="pass",name[]="thee",surename[]="sahawat",direction1[]="access_sever",direction4[]="yes",direction6[]="on",direction8[]="access_helper",direction9[]="help",direction10[]="access_calculator",direction11[]="exit";
+  char cal1[]="plus",cal2[]="minus",direction13[]="cam",direction14[]="exit",direction15[]="out",direction16[]="logout",direction17[]="access_pjmm",direction18[]="access_dmm",direction_yes[]="yes",direction_no[]="no";
+  char cal[20],ps[20],direction_dmm[20];
+  int i=0;
+  float num1,num2,num3,num4;
+do{
+   log:
+  printf("--------S.H.A.P login system--------\n");
+  printf("if you want to exit\nusername and password = exit\n\n");
+  printf("username:");
+  scanf("%s",user);
+  printf("password:");
+   scanf("%s",pass);
+  if(strcmp(user,user1)==0 && strcmp(pass,pass1)==0)
+   {   
+       system("cls");
+   	   printf("login successful\nwelcome to S.H.A.P system\n");
+   	   printf("if you don't know command enter help\n\n");
+       i=3;
+       start:
+       printf("what do you want to do:");
+       scanf("%s",direction2);
+       if(strcmp(direction2,direction1)==0)
+       {
+       	//sever
+       	system("cls");
+         printf("-----welcome to S.H.S-----\n");
+         printf("if you want to turn on S.H.S text on:");
+         scanf("%s",direction5);
+         if (strcmp(direction5,direction6)==0)
+         {
+         system("pause");
+         system("cls");
+         printf("SEVER STAT:STARTING\n");
+         sleep(6);
+         system("cls");
+         printf("SEVER STAT:HALF RUNING\n");
+         system("pause");
+         system("cls");
+         printf("SYSTEM CHEACK LIST\n");
+         system("pause");
+         system("cls");
+         printf("HARDWARE CHEACK:");
+         sleep(4);
+         system("cls");
+         printf("HARDWARE CHEACK:PASSED\n");
+         printf("SOFEWARE CHEACK:");
+         sleep(4);
+         system("cls");
+         printf("HARDWARE CHEACK:PASSED\n");
+         printf("SOFEWARE CHEACK:PASSED\n");
+         printf("STORAGE CHEACK:");
+         sleep(4);
+         system("cls");
+         printf("HARDWARE CHEACK:PASSED\n");
+         printf("SOFEWARE CHEACK:PASSED\n");
+         printf("STORAGE CHEACK:PASSED\n");
+         printf("NETWORK CHEACK:");
+         sleep(4);
+         system("cls");
+         printf("HARDWARE CHEACK:PASSED\n");
+         printf("SOFEWARE CHEACK:PASSED\n");
+         printf("STORAGE CHEACK:PASSED\n");
+         printf("NETWORK CHEACK:PASSED\n");
+         system("pause");
+         system("cls");
+         printf("SYSTEM CHEACK:OK\n");
+         printf("SYSTEM STAT:FULL RUNING\n");
+         system("pause");
+         system("cls");
+         goto start;
+		 }
+		 else
+		 {
+		  system("cls");
+		  goto start;
+		 }
+       }
+       else if (strcmp(direction2,direction17)==0)
+       {
+       	//P.J.M.M
+       	system("cls");
+       	printf("WARNING THIS PROGRAM IS FOR ADMIN ONLY\n");
+       	printf("name:");
+       	scanf("%s",name1);
+       	printf("surename:");
+       	scanf("%s",surename1);
+       	if (strcmp(name,name1)==0 && strcmp(surename,surename1)==0)
+       	{
+       	pjmm:
+       	system("cls");
+       	printf("hi skye and welcome\n");
+       	printf("P .J  . M    . M\n");
+       	printf("PROJECT MANAGE MENT\n");
+       	printf("new project count:1\n");
+       	printf("code list\n");
+       	printf("1.access_dmm = door manage ment\n");
+       	printf("2.out = exit this program\n");
+       	printf("3.exit = exit\n");
+       	printf("project selected:");
+       	scanf("%s",ps);
+       	if (strcmp(ps,direction18)==0)
+       	{
+       	system("cls");
+       	printf("welcome to\n");
+       	printf("D  . M    . M\n"); 
+       	printf("DOOR MANAGE MENT\n");
+       	dmm:
+       	printf("do you want to open-close door:");
+       	scanf("%s",direction_dmm);
+       	if(strcmp(direction_dmm,direction_yes)==0)
+       	{
+       	system("cls");
+       	printf("door moveing\n");
+		sleep(4);
+		printf("door moved\n");
+		system("pause");
+		system("cls");
+		goto start;	
+		}
+		else if (strcmp(direction_dmm,direction_no)==0)
+		{
+		system("cls");
+		printf("ok\n");
+		system("pause");
+		system("cls");
+		goto start;
+		}
+		else
+		{
+		system("cls");
+		printf("command not found\n");
+		system("pause");
+		system("cls");
+		goto dmm;
+	    }
+		}
+		else if (strcmp(ps,direction14)==0)
+		{
+		system("cls");
+		return 0;
+		}
+		else if (strcmp(ps,direction15)==0)
+	    {
+	    system("cls");
+		goto start;
+	    }
+		}
+	   else
+	   {
+	   	printf("project not found\n");
+	   	system("pause");
+	   	goto pjmm;
+	   }
+       } 
+       else if (strcmp(direction2,direction14)==0)
+       {
+       	system("cls");
+       	return 0;
+	   }
+       else if (strcmp(direction2,direction8)==0)
+       {
+       	//helper
+       system("cls");
+       printf("WARNING:THIS SYSTEM IS FOR ADMIN ONLY\n");
+       printf("name:");
+       scanf("%s",name1);
+       printf("surename:");
+       scanf("%s",surename1);
+       if (strcmp(name,name1)==0 && strcmp(surename,surename1)==0)
+       {
+       system("cls");
+       printf("helper:good morning/evening skye\n");
+       printf("and welcome to\n");
+       printf("HUMAN EDUCATION AND LANGUAGE PROTOCOL ELEMENT ROUTER\n");
+       printf("H   . E       .     L      . P      . E     . R\n");
+       help:
+       printf("\ncode list\n");
+       printf("1.cam = open camera\n2.out = exit this program\n3.exit = exit\n");
+       printf("command:");
+       scanf("%s",direction12);
+       if (strcmp(direction12,direction13)==0)
+       {
+       system("cls");
+       printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n                                    camera not found                                     \n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+       system("pause");
+       system("cls");
+       goto help;
+	   }
+	   //exit
+	   else if (strcmp(direction12,direction14)==0)
+	   {
+	   	system("cls");
+	   	return 0;
+	   }
+	   else if (strcmp(direction12,direction15)==0)
+	   {
+	   	system("cls");
+	   	goto start;
+	   }
+	   else
+	   {
+	   	system("cls");
+	   	printf("command not found");
+	   	system("pause");
+	   	goto help;
+	   }
+	   }
+	   else
+	   {
+	   system("cls");
+	   printf("you don't have permission to access this program\n");
+	   system("pause");
+	   system("cls");
+	   goto start;
+	   }
+	   }
+	   else if(strcmp(direction2,direction9)==0)
+	   {
+	   	//help
+	   system("cls");
+	   printf("----welcome to help terminal----\n");
+	   printf("code list\n");
+	   printf("1.access_sever = start S.H.S\n");
+	   printf("2.access_calculator = run calculator\n");
+	   printf("3.access_helper = run H.E.L.P.E.R (admin only)\n");
+	   printf("4.access_pjmm = run P.J.M.M (admin only)\n");
+	   printf("5.access_dmm = run door manage ment\n");
+	   printf("6.logout = logout\n");
+	   printf("7.exit = exit\n");
+	   system("pause");
+	   system("cls");
+	   goto start;
+	   }
+	   else if (strcmp(direction2,direction10)==0)
+	   {
+	   	//calculator
+	   	system("cls");
+	   	printf("----welcome to calculator----\n");
+	   	printf("WARNING:this calculator can only plus-minus\n");
+	   	system("pause");
+	   	system("cls");
+	   	cal1:
+	   	printf("do you want to plus or minus:");
+	   	scanf("%s",cal);
+	   	if(strcmp(cal,cal1)==0)
+	   	{
+	   	system("cls");
+	   	printf("plus\n");
+		printf("number1:");
+		scanf("%f",&num1);
+		printf("number2:");
+		scanf("%f",&num2);
+		float num3=num1+num2;
+		printf("sum = %f\n",num3);
+		system("pause");
+		system("cls");
+		goto start;	
+		}
+		else if (strcmp(cal,cal2)==0)
+		{
+		system("cls");
+		printf("minus\n");
+		printf("number1:");
+		scanf("%f",&num1);
+		printf("number2:");
+		scanf("%f",&num2);
+		float num3=num1-num2;
+		printf("sum = %f\n",num3);
+		system("pause");
+		system("cls");
+		goto start;
+		}
+		else
+		{
+		system("cls");
+		printf("command not found\n");
+		system("pause");
+		system("cls");
+		goto cal1;
+		}
+	    }
+	   else if (strcmp(direction2,direction11)==0)
+	   {
+	   	system("cls");
+	   printf("error:command not found");
+	   return 0;
+	   }
+	   else if (strcmp(direction2,direction16)==0)
+	   {
+	   	system("cls");
+	   	goto log;
+	   }
+	    else if (strcmp(direction2,direction18)==0)
+       	{
+       	system("cls");
+       	printf("welcome to\n");
+       	printf("D  . M    . M\n"); 
+       	printf("DOOR MANAGE MENT\n");
+       	dmm2:
+       	printf("do you want to open-close door:");
+       	scanf("%s",direction_dmm);
+       	if(strcmp(direction_dmm,direction_yes)==0)
+       	{
+       	system("cls");
+       	printf("door moveing\n");
+		sleep(4);
+		printf("door moved\n");
+		system("pause");
+		system("cls");
+		goto start;	
+		}
+		else if (strcmp(direction_dmm,direction_no)==0)
+		{
+		system("cls");
+		printf("ok\n");
+		system("pause");
+		system("cls");
+		goto start;
+		}
+		else
+		{
+		system("cls");
+		printf("command not found\n");
+		system("pause");
+		system("cls");
+		goto dmm2;
+	    }
+	    }
+	   else
+	   {
+	   printf("command not found\n");
+	   system ("pause");
+	   system("cls");
+	   goto start;
+	   }
+   }
+   else if (strcmp(user,direction14)==0 && strcmp(pass,direction14)==0)
+   {
+   system("cls");
+   return 0;
+   }
+  else
+  {
+  	  {
+  	 system("cls");
+     printf("error:username or password not found\n!!please try again!! %d/3\n",i+1);
+     if(i<2)
+	 {
+      system("pause");
+      system("cls");
+	 }
+	 else
+	 {
+	 system("cls");
+     printf("access denied %d/3",i+1);
+    }
+      }
+  }
+  i++;
+}
+while(i<3);
+}
